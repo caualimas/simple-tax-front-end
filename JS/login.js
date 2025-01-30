fdbf3f0ba761f3816c6d4980c1a322b2ac8bbbb0
jpfabiano <jpfabiano08@gmail.com>
2025-01-30 13:03:57 -0300

    trying to implement cookies

--- JS/login.js
+++ JS/login.js
@@ -1,6 +1,7 @@
 const buttomEntrar = document.getElementById("entrar");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
+const buttomTeste = document.getElementById("teste");
 
 buttomEntrar.addEventListener('click', (event) => {
     event.preventDefault();  // Previne o envio padrão do formulário
@@ -16,13 +17,42 @@ buttomEntrar.addEventListener('click', (event) => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username: valueUsername, password: valuePassword })
+        body: JSON.stringify({ usernameOrEmail: valueUsername, password: valuePassword }),
+        credentials: 'include'
     })
-    .then(response => response.json()) 
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erro HTTP! Status: ${response.status}`);
+        }
+        return response.text(); // Usa .text() ao invés de .json()
+    })
     .then(data => {
-        console.log('Resposta do servidor:', data);
+        try {
+            const jsonData = JSON.parse(data); // Tenta converter em JSON
+            console.log('Resposta JSON:', jsonData);
+        } catch (error) {
+            console.log('Resposta não é JSON:', data); // Se não for JSON, apenas printa
+        }
     })
     .catch(error => {
         console.error('Erro ao enviar os dados:', error);
     });
 });
+
+buttomTeste.addEventListener('click', (event) => {
+    event.preventDefault();
+
+
+    fetch('http://localhost:8080/produto', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            
+        },
+        credentials: 'include' // Inclui cookies na requisição
+    })
+    .then(response => response.json()) 
+    .then(data => console.log(data))
+    .catch(error => console.error('Erro:', error));
+    
+})
